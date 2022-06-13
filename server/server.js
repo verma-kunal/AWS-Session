28b0b57ebaab770044307306bb27baf590a6c000
Kunal Verma <vkunal321@gmail.com>
2022-06-13 14:27:08 +0530

    Add: Basic server 2.0

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const express = require('express')
+const stripe = require('stripe')(process.env.STRIPE_PUBLISHABLE_KEY)
+require('dotenv').config({ path: './.env' });
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+// ----------- Imports & necessary things here -----------
 
 // Setting up the static folder here:
 app.use('/client', express.static(__dirname + "/../client"));
