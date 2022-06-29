eef06ab6ea477e41a8dc16319ca85f63380a5c6f
Kunal Verma <vkunal321@gmail.com>
2022-06-29 11:24:46 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -6,7 +6,7 @@ var btnWorshop3 = document.getElementById("work-3");
 
 // __________________________________________________________________
 
-var URL = "https://kubesimplify-workshop.herokuapp.com";
+var URL = "https://kube-workshop.herokuapp.com";
 
 btnWorshop1.addEventListener("click", function (e) {
   e.preventDefault();
