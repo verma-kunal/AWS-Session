0fe93aaa59f56235dcd02693ee5cd0d974705f76
Ubuntu <ubuntu@ip-172-31-11-238.ap-south-1.compute.internal>
2022-11-11 15:44:35 +0000

    Update code

--- server.js
+++ server.js
@@ -78,4 +78,5 @@ app.post("/create-checkout-session/:pid", async (req, res) => {
 // Server listening:
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
+  console.log('You may access you app at: http://13.235.151.196:3000/');
 });
